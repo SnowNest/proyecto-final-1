e2e2e12430183b0195f5c39dc4366ea15223d42f
daton <rapidclimate@gmail.com>
2017-07-17 16:24:31 -0500

    sdsdsdsd

--- src/main/java/org/campitos/fin/ControladorMensaje.java
+++ src/main/java/org/campitos/fin/ControladorMensaje.java
@@ -5,6 +5,8 @@
  */
 package org.campitos.fin;
 
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class ControladorMensaje {
     
-   
+   @Autowired RepositorioMensaje repoMensaje;
     //caso post 
     @RequestMapping(value="/mensaje", method=RequestMethod.POST,
             headers={"application/json"})
@@ -26,6 +28,11 @@ public class ControladorMensaje {
         Estatus e=new Estatus();
         e.setSuccess(true);
         return e;
+    }
+    @RequestMapping(value="/mensaje", method = RequestMethod.GET, headers = {"Accept=application/json"})
+    public ArrayList<Mensaje> getTodos(){
+         
+        return (ArrayList<Mensaje>) repoMensaje.findAll();
     }
     
 }
